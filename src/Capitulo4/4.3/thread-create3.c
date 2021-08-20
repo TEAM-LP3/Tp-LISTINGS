f7c9a0cbb02dc09a9a4d528061596e835660aa81
gisselle96 <gibasa96@gmail.com>
2021-08-20 03:05:38 -0400

    Modificando el listing 4.3 del capitulo 4

--- src/Capitulo4/4.3/thread-create3.c
+++ src/Capitulo4/4.3/thread-create3.c
@@ -1,44 +1,44 @@
 #include <pthread.h>
 #include <stdio.h>
-/* Parameters to print_function. */
+/* Parametros para print_function. */
 struct char_print_parms
 {
-    /* The character to print. */
+    /*El caracter a imprimir */
     char character;
-    /* The number of times to print it. */
+    /* El numero de veces que se imprimira */
     int count;
 };
-/* Prints a number of characters to stderr, as given by PARAMETERS,
-which is a pointer to a struct char_print_parms. */
+/* Imprime una serie de caracteres a stderr, según lo indicado por PARAMETERS,
+que es un puntero a una estructura char_print_parms. */
 void *char_print(void *parameters)
 {
-    /* Cast the cookie pointer to the right type. */
+    /* Lanza el puntero de la cookie al tipo correcto. */
     struct char_print_parms *p = (struct char_print_parms *)parameters;
     int i;
     for (i = 0; i < p->count; ++i)
         fputc(p->character, stderr);
     return NULL;
 }
-/* The main program. */
+/* El main del programa. */
 int main()
 {
     pthread_t thread1_id;
     pthread_t thread2_id;
     struct char_print_parms thread1_args;
     struct char_print_parms thread2_args;
-    /* Create a new thread to print 30,000 x’s. */
+    /* Crea un nuevo hilo para imprimir  30,000 x’s. */
     thread1_args.character = 'x';
     thread1_args.count = 30000;
     pthread_create(&thread1_id, NULL, &char_print, &thread1_args);
-    /* Create a new thread to print 20,000 o’s. */
+    /* Crea un nuevo hilo para imprimir 20,000 o’s. */
     thread2_args.character = 'o';
     thread2_args.count = 20000;
     pthread_create(&thread2_id, NULL, &char_print, &thread2_args);
-    /* Make sure the first thread has finished. */
+    /* Asegura que el primer hilo haya terminado. */
     pthread_join(thread1_id, NULL);
-    /* Make sure the second thread has finished. */
+    /* Asegura que el segundo hilo haya terminado. */
     pthread_join(thread2_id, NULL);
     printf("\nMain thread saliendo luego de esperar a los dos hilos\n");
-    /* Now we can safely return. */
+    /* Ahora retorna.*/
     return 0;
 }
