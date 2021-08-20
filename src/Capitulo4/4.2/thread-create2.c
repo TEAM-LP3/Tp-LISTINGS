ac5a419eee67c6c0bdd769a05e94f784e6741002
gisselle96 <gibasa96@gmail.com>
2021-08-20 03:05:01 -0400

    Modificando el listing 4.2 del capitulo 4

--- src/Capitulo4/4.2/thread-create2.c
+++ src/Capitulo4/4.2/thread-create2.c
@@ -1,36 +1,36 @@
 #include <pthread.h>
 #include <stdio.h>
-/* Parameters to print_function. */
+/* Parametros para print_function. */
 struct char_print_parms
 {
-    /* The character to print. */
+    /* El caracter que sera imprimido. */
     char character;
-    /* The number of times to print it. */
+    /* El numero de veces que se imprimira */
     int count;
 };
-/* Prints a number of characters to stderr, as given by PARAMETERS,
-which is a pointer to a struct char_print_parms. */
+/* Imprime una serie de caracteres en stderr, según lo indicado por PARAMETERS,
+que es un puntero a una estructura char_print_parms. */
 void *char_print(void *parameters)
 {
-    /* Cast the cookie pointer to the right type. */
+    /* lanza el puntero de la cookie al tipo correcto. */
     struct char_print_parms *p = (struct char_print_parms *)parameters;
     int i;
     for (i = 0; i < p->count; ++i)
         fputc(p->character, stderr);
     return NULL;
 }
-/* The main program. */
+/* El main del programa. */
 int main()
 {
     pthread_t thread1_id;
     pthread_t thread2_id;
     struct char_print_parms thread1_args;
     struct char_print_parms thread2_args;
-    /* Create a new thread to print 30,000 ’x’s. */
+    /* Crea un nuevo hilo para imprimir 30,000 ’x’s. */
     thread1_args.character = 'x';
     thread1_args.count = 30000;
     pthread_create(&thread1_id, NULL, &char_print, &thread1_args);
-    /* Create a new thread to print 20,000 o’s. */
+    /* CCrea un nuevo hilo para imprimir 20,000 o’s. */
     thread2_args.character = 'o';
     thread2_args.count = 20000;
     pthread_create(&thread2_id, NULL, &char_print, &thread2_args);
