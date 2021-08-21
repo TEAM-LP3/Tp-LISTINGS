5bcf3ba944dcce76bd5809828bd85e34fd86254a
gisselle96 <gibasa96@gmail.com>
2021-08-21 00:02:15 -0400

    Modificando el ejercicio3.4 del capitulo3

--- src/Capitulo3/3.4/fork-exec.c
+++ src/Capitulo3/3.4/fork-exec.c
@@ -2,34 +2,34 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
-/* Spawn a child process running a new program. PROGRAM is the name
-of the program to run; the path will be searched for this program.
-ARG_LIST is a NULL-terminated list of character strings to be
-passed as the program’s argument list. Returns the process ID of
-the spawned process. */
+/* Genera un proceso hijo que ejecuta un nuevo programa. PROGRAMA es el nombre
+del programa a ejecutar; se buscará la ruta para este programa.
+ARG_LIST es una lista terminada en NULL de cadenas de caracteres que se
+pasado como la lista de argumentos del programa. Devuelve el ID de proceso de
+el proceso engendrado. */
 int spawn(char *program, char **arg_list)
 {
     pid_t child_pid;
-    /* Duplicate this process. */
+    /* Duplica este proceso */
     child_pid = fork();
     if (child_pid != 0)
-        /* This is the parent process. */
+        /* Este es el proceso principal*/
         return child_pid;
     else
     {
-        /* Now execute PROGRAM, searching for it in the path. */
+        /* Ahora ejecuta PROGRAM, buscándolo en la ruta. */
         execvp(program, arg_list);
-        /* The execvp function returns only if an error occurs. */
+        /* La función execvp regresa solo si ocurre un error. */
         fprintf(stderr, "an error occurred in execvp\n");
         abort();
     }
 }
 int main()
 {
-    /* The argument list to pass to the “ls” command. */
-    char *arg_list[] = {"ls", /* argv[0], the name of the program. */ "-l", "/", NULL /* The argument list must end with a NULL. */};
-    /* Spawn a child process running the “ls” command. Ignore the
-returned child process ID. */
+    /* La lista de argumentos para pasar al comando "ls". */
+    char *arg_list[] = {"ls", /* argv[0], el nombre del program */ "-l", "/", NULL /* La lista de argumentos debe terminar con NULL. */};
+    /* Genera un proceso hijo que ejecute el comando "ls". Ignora el
+        ID de proceso hijo devuelto. */
     spawn("ls", arg_list);
     printf("done with main program\n");
     return 0;
