88c6f72d25c6241ab86244b257ed98836cd65271
fabriziozero <fabriziozero13@gmail.com>
2021-08-25 13:54:01 -0400

    Listing 2.9 - tifftest.c e imagen tiff

---
+++ src/Capitulo2/2.9/tifftest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <tiffio.h>
+#include <assert.h>
+
+int main(int argc, char **argv)
+{
+    assert(argv[1]!=NULL);
+    TIFF *tiff;
+    tiff = TIFFOpen(argv[1], "r");
+    printf("Img.tiff abierta");
+    TIFFClose(tiff);
+    return 0;
+}
